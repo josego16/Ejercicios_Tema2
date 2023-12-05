272216f481a57949655dc3178c22050030f7f775
josego16 <jgomlin@g.educaand.es>
2023-12-05 19:35:27 +0100

    hemos realizado el ejercicio 7 con exito

--- src/main/java/Ejercicio7/mainColecta.java
+++ src/main/java/Ejercicio7/mainColecta.java
@@ -4,16 +4,12 @@ public class mainColecta {
     public static void main(String[] args) {
         colecta colecta = new colecta();
 
-        hilos hilo1 = new hilos(colecta, "Hilo 1");
-        hilos hilo2 = new hilos(colecta, "Hilo 2");
-        hilos hilo3 = new hilos(colecta, "Hilo 3");
-        hilos hilo4 = new hilos(colecta, "Hilo 4");
-        hilos hilo5 = new hilos(colecta, "Hilo 5");
-
-        hilo1.start();
-        hilo2.start();
-        hilo3.start();
-        hilo4.start();
-        hilo5.start();
+        System.out.println("--- Huerta de Jose Maria ---");
+        System.out.println("Inicio de la recolecta");
+        System.out.println("----------------------------\n");
+        for (int i = 1; i <= 4; i++) {
+            hilos hilo = new hilos(colecta, "Hilo " + i);
+            hilo.start();
+        }
     }
 }
